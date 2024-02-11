4dee7fe6d64c37d2ed13eaac6d646160f0e027d5
AnetSow <a.sowinska27@gmail.com>
2024-02-11 18:33:41 +0100

    Products barchart added

--- src/components/BarCharts/ProductsBarchart.jsx
+++ src/components/BarCharts/ProductsBarchart.jsx
@@ -1,3 +1,52 @@
-import * as d3 from "d3";
-import Product from "../Product/Product";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+export const ProductsBarchart = (props) => {
+
+    const { titles, prices} = props
+
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Prices for products',
+          },
+        },
+      };
+      
+      const data = {
+        labels: titles,
+        datasets: [
+          {
+            data: prices,
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+        ],
+      };
+      
+    return(
+        < Bar options={options} data={data} />
+    )
+}
 
